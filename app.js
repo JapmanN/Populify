6011abe44361ca817e4b7c5c5ee90fd2521cdff8
JapmanN <japmannagra20@gmail.com>
2019-09-07 20:58:38 +0000

    get an access token and redirect user to main page of application

--- app.js
+++ app.js
@@ -1,8 +1,51 @@
+// ================
+// REQUIRE PACKAGES
+// ================
 const express       = require("express"),
 	  app           = express(),
 	  SpotifyWebApi = require('spotify-web-api-node');
 
+// =================
+// APPLICATION SETUP
+// =================
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
+// ================
+// APPLICATION CODE
+// ================
+var spotifyApi = new SpotifyWebApi({
+	scopes: ['user-read-private', 'user-read-email'],
+	redirectUri: 'REDIRECT_URI',
+	clientId: 'CLIENT_ID',
+	clientSecret: 'CLIENT_SECRET',
+	state: 'mySTATE'
+});
+
+// Create Authorization URL
+var authorizeURL = spotifyApi.createAuthorizeURL(spotifyApi._credentials.scopes, spotifyApi._credentials.state);
+
+app.get('/login', (req, res) => {
+	res.redirect(authorizeURL);
+});
+
+app.get('/callback', (req, res) => {
+	var code = req.query.code;	
+	// Retrieve an access token and a refresh token
+	spotifyApi.authorizationCodeGrant(code).then(function(data) {
+		// Set the access token on the API object to use it in later calls
+		spotifyApi.setAccessToken(data.body['access_token']);
+		spotifyApi.setRefreshToken(data.body['refresh_token']);
+		res.redirect('/app');
+	}, function(err) {
+		console.log('Something went wrong!', err);
+	});
+});
+
+app.get('/app', (req, res) => {
+	res.send("Does this thing work?");
+});
 
 // ==============
 // SERVER STARTUP
